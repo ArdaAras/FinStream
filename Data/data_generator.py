f1173088b50e8583843d69e3ccdd4a3148978457
Arda <arda.aras.dev@gmail.com>
2024-10-06 20:53:01 +0300

    Added files that will be used in data generation
    + Added data_generator and definitions scripts.
    + Added merchants.txt file which will be used during execution.
    + Added sample_transactions.txt for sample-generated data

---
+++ Data/data_generator.py
@@ -0,0 +1,43 @@
+# This file generates random data 
+
+from definitions import customer_ids, transaction_types, account_types, currencies, merchants, transaction_statuses, fake_generator, random
+
+def generate_financial_transaction(num_records: int = 10) -> list:
+    """
+    Generates a list of random financial transactions and returns list 
+    """
+    financial_data = []
+
+    for _ in range(num_records):    
+        # Randomly generate transaction details
+        transaction_id = fake_generator.uuid4()
+        customer_id = random.choice(customer_ids)
+        transaction_amount = round(random.uniform(-5000, 5000), 2)  # Debits are negative, credits are positive
+        transaction_type = random.choice(transaction_types)
+        account_type = random.choice(account_types)
+        transaction_date = fake_generator.date_time_this_year()
+        transaction_status = random.choice(transaction_statuses)
+        currency = random.choice(currencies)
+        merchant = random.choice(merchants)
+        
+        # Simulate negative amounts for debits and positive for credits
+        if transaction_type in ['debit', 'withdrawal']:
+            transaction_amount = abs(transaction_amount) * -1
+        else:
+            transaction_amount = abs(transaction_amount)
+
+        record = {
+            'transaction_id': transaction_id,
+            'customer_id': customer_id,
+            'transaction_amount': transaction_amount,
+            'transaction_type': transaction_type,
+            'account_type': account_type,
+            'transaction_date': transaction_date.isoformat(),
+            'transaction_status' : transaction_status,
+            'currency': currency,
+            'merchant': merchant,
+        }
+
+        financial_data.append(record)
+
+    return financial_data
---
+++ Data/definitions.py
@@ -0,0 +1,25 @@
+# This file holds data definitions
+
+import random as random
+from faker import Faker
+
+fake_generator = Faker()
+
+def load_merchants() -> list:
+    """
+    Reads the merchants from a file and returns a list of merchants.
+    """
+    with open('.\Dataset\merchants.txt', 'r') as file:
+        lines = file.readlines()
+
+    # Remove newline characters
+    lines = [line.strip() for line in lines]
+
+    return lines
+
+customer_ids = [random.randint(1,1000) for _ in range(1000)]
+transaction_types = ['debit', 'credit', 'transfer', 'withdrawal']
+account_types = ['savings', 'checking', 'credit card', 'investment']
+transaction_statuses = ["Success", "Failed", "Pending"]
+currencies = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD']
+merchants = load_merchants()
